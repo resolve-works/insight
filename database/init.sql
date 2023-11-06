74f0c8b12b95c7c98502b78d5d17fa466f30df03
Johan <johan@ftm.nl>
2023-11-06 02:34:50 +0100

    let nginx handle uploads, pass on details to api

--- database/init.sql
+++ database/init.sql
@@ -14,22 +14,21 @@ grant web_user to :authenticator;
 create schema insight;
 grant usage on schema insight to web_user;
 
-create table insight.upload (
+create table insight.uploads (
     id uuid not null default gen_random_uuid(),
     name text not null,
     primary key (id)
 );
-grant all on insight.upload to web_user;
+grant all on insight.uploads to web_user;
 
-create table insight.file (
+create table insight.files (
     id uuid not null default gen_random_uuid(),
     upload_id uuid not null,
     name text not null,
 
     primary key (id),
-    foreign key(upload_id) references insight.upload (id) match simple on delete restrict not valid
+    foreign key(upload_id) references insight.uploads (id) match simple on delete restrict not valid
 );
-
-grant all on insight.file to web_user;
+grant all on insight.files to web_user;
 
 commit;
