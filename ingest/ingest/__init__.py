b00352c09568637557cfd018b056ebc669958290
Johan <johan@ftm.nl>
2023-11-07 19:18:24 +0100

    autorestart ingest in development

--- ingest/ingest/__init__.py
+++ ingest/ingest/__init__.py
@@ -1,9 +1,13 @@
 import click
-import psycopg2
+import json
 import asyncio
 import os
 import logging
-from sqlalchemy import create_engine
+from pathlib import Path
+from pikepdf import Pdf
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+from .models import Pagestream, File
 
 logging.basicConfig(level=logging.INFO)
 
@@ -14,17 +18,57 @@ engine = create_engine(
 )
 
 conn = engine.connect()
-
-cursor = conn.cursor()
-cursor.execute("listen pagestreams;")
+conn.execute(text("listen pagestream; listen file;"))
 conn.commit()
 
 
-def handle_notify():
-    conn.poll()
-    for notify in conn.notifies:
-        logging.info(notify.payload)
-    conn.notifies.clear()
+def process_pagestream(id, path, name):
+    logging.info(f"Ingesting pagestream {id}")
+
+    with Pdf.open(path) as pdf:
+        count = len(pdf.pages)
+
+    with Session(engine) as session:
+        file = File(
+            name=name,
+            first_page=0,
+            last_page=count - 1,
+            pagestream_id=id,
+        )
+
+        session.add(file)
+        session.commit()
+
+
+def process_file(id, pagestream_id, first_page, last_page, name):
+    logging.info(f"Ingesting file {id}")
+
+    with Session(engine) as session:
+        pagestream = session.query(Pagestream).get(pagestream_id)
+
+    # Split PDF from pagestream
+    destination = Pdf.new()
+    with Pdf.open(pagestream.path) as pdf:
+        for page in pdf.pages[first_page:last_page]:
+            destination.pages.append(page)
+        destination.copy_foreign(pdf.docinfo)
+        destination.save(Path(os.environ.get("INGEST_FILES_PATH")) / f"{id}.pdf")
+
+    # OCR & optimize new PDF
+
+
+def reader():
+    conn.connection.poll()
+    for notification in conn.connection.notifies:
+        object = json.loads(notification.payload)
+
+        match notification.channel:
+            case "pagestream":
+                process_pagestream(**object)
+            case "file":
+                process_file(**object)
+
+    conn.connection.notifies.clear()
 
 
 @click.group()
@@ -36,5 +80,5 @@ def cli():
 def process_messages():
     logging.info("Processing messages")
     loop = asyncio.get_event_loop()
-    loop.add_reader(conn, handle_notify)
+    loop.add_reader(conn.connection, reader)
     loop.run_forever()
--- ingest/ingest/models.py
+++ ingest/ingest/models.py
@@ -1,35 +1,52 @@
 from typing import List
 
-from sqlalchemy import ForeignKeyConstraint, PrimaryKeyConstraint, Text, Uuid, text
+from sqlalchemy import (
+    ForeignKeyConstraint,
+    Integer,
+    PrimaryKeyConstraint,
+    Text,
+    Uuid,
+    text,
+)
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 import uuid
+
 
 class Base(DeclarativeBase):
     pass
 
 
-class Pagestreams(Base):
-    __tablename__ = 'pagestreams'
-    __table_args__ = (
-        PrimaryKeyConstraint('id', name='pagestreams_pkey'),
-    )
+class Pagestream(Base):
+    __tablename__ = "pagestream"
+    __table_args__ = (PrimaryKeyConstraint("id", name="pagestream_pkey"),)
 
-    id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, server_default=text('gen_random_uuid()'))
+    id: Mapped[uuid.UUID] = mapped_column(
+        Uuid, primary_key=True, server_default=text("gen_random_uuid()")
+    )
     path: Mapped[str] = mapped_column(Text)
     name: Mapped[str] = mapped_column(Text)
 
-    files: Mapped[List['Files']] = relationship('Files', back_populates='pagestream')
+    file: Mapped[List["File"]] = relationship("File", back_populates="pagestream")
 
 
-class Files(Base):
-    __tablename__ = 'files'
+class File(Base):
+    __tablename__ = "file"
     __table_args__ = (
-        ForeignKeyConstraint(['pagestream_id'], ['pagestreams.id'], ondelete='RESTRICT', name='files_pagestream_id_fkey'),
-        PrimaryKeyConstraint('id', name='files_pkey')
+        ForeignKeyConstraint(
+            ["pagestream_id"],
+            ["pagestream.id"],
+            ondelete="RESTRICT",
+            name="file_pagestream_id_fkey",
+        ),
+        PrimaryKeyConstraint("id", name="file_pkey"),
     )
 
-    id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, server_default=text('gen_random_uuid()'))
+    id: Mapped[uuid.UUID] = mapped_column(
+        Uuid, primary_key=True, server_default=text("gen_random_uuid()")
+    )
     pagestream_id: Mapped[uuid.UUID] = mapped_column(Uuid)
+    first_page: Mapped[int] = mapped_column(Integer)
+    last_page: Mapped[int] = mapped_column(Integer)
     name: Mapped[str] = mapped_column(Text)
 
-    pagestream: Mapped['Pagestreams'] = relationship('Pagestreams', back_populates='files')
+    pagestream: Mapped["Pagestream"] = relationship("Pagestream", back_populates="file")
