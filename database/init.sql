2a3001969a80d8214afad579e91211614b12b44c
Johan <johan@ftm.nl>
2023-11-08 18:05:29 +0100

    rename, clean up, add page

--- database/init.sql
+++ database/init.sql
@@ -4,6 +4,8 @@ begin;
 \set pg_api_password `echo $PG_API_PASSWORD`
 \set pg_ingest_user `echo $PG_INGEST_USER`
 \set pg_ingest_password `echo $PG_INGEST_PASSWORD`
+\set pg_worker_user `echo $PG_WORKER_USER`
+\set pg_worker_password `echo $PG_WORKER_PASSWORD`
 \set web_anon `echo $PG_ANONYMOUS`
 
 create schema insight;
@@ -17,9 +19,12 @@ grant web_user to :pg_api_user;
 
 create role :pg_ingest_user noinherit login password :'pg_ingest_password';
 alter role :pg_ingest_user set search_path = "$user", insight;
+create role :pg_worker_user noinherit login password :'pg_worker_password';
+alter role :pg_worker_user set search_path = "$user", insight;
 
 grant usage on schema insight to web_user;
 grant usage on schema insight to :pg_ingest_user;
+grant usage on schema insight to :pg_worker_user;
 
 create or replace function notify()
 returns trigger language plpgsql as
@@ -58,6 +63,23 @@ grant all on insight.file to :pg_ingest_user;
 
 create trigger notify_file
 after insert on insight.file for each row 
+execute function notify();
+
+create table insight.page (
+    id uuid not null default gen_random_uuid(),
+    pagestream_id uuid not null,
+    index integer not null,
+    content text,
+
+    primary key (id),
+    foreign key(pagestream_id) references insight.pagestream (id) match simple on delete restrict not valid
+);
+grant all on insight.page to web_user;
+grant all on insight.page to :pg_ingest_user;
+grant all on insight.page to :pg_worker_user;
+
+create trigger notify_page
+after insert on insight.page for each row 
 execute function notify();
 
 commit;
--- ingest/insight_ingest/models.py
+++ ingest/insight_ingest/models.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from sqlalchemy import ForeignKeyConstraint, Integer, PrimaryKeyConstraint, Text, Uuid, text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
@@ -19,6 +19,7 @@ class Pagestream(Base):
     name: Mapped[str] = mapped_column(Text)
 
     file: Mapped[List['File']] = relationship('File', back_populates='pagestream')
+    page: Mapped[List['Page']] = relationship('Page', back_populates='pagestream')
 
 
 class File(Base):
@@ -35,3 +36,18 @@ class File(Base):
     name: Mapped[str] = mapped_column(Text)
 
     pagestream: Mapped['Pagestream'] = relationship('Pagestream', back_populates='file')
+
+
+class Page(Base):
+    __tablename__ = 'page'
+    __table_args__ = (
+        ForeignKeyConstraint(['pagestream_id'], ['pagestream.id'], ondelete='RESTRICT', name='page_pagestream_id_fkey'),
+        PrimaryKeyConstraint('id', name='page_pkey')
+    )
+
+    id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, server_default=text('gen_random_uuid()'))
+    pagestream_id: Mapped[uuid.UUID] = mapped_column(Uuid)
+    index: Mapped[int] = mapped_column(Integer)
+    content: Mapped[Optional[str]] = mapped_column(Text)
+
+    pagestream: Mapped['Pagestream'] = relationship('Pagestream', back_populates='page')
