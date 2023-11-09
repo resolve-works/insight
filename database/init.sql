4fdddd3391160020ee7fe9c1d679ec3e04d77408
Johan <johan@ftm.nl>
2023-11-09 14:25:31 +0100

    pass ocrd docs to tika

--- database/init.sql
+++ database/init.sql
@@ -2,8 +2,6 @@ begin;
 
 \set pg_api_user `echo $PG_API_USER`
 \set pg_api_password `echo $PG_API_PASSWORD`
-\set pg_ingest_user `echo $PG_INGEST_USER`
-\set pg_ingest_password `echo $PG_INGEST_PASSWORD`
 \set pg_worker_user `echo $PG_WORKER_USER`
 \set pg_worker_password `echo $PG_WORKER_PASSWORD`
 \set web_anon `echo $PG_ANONYMOUS`
@@ -17,13 +15,10 @@ create role :pg_api_user noinherit login password :'pg_api_password';
 grant :web_anon to :pg_api_user;
 grant web_user to :pg_api_user;
 
-create role :pg_ingest_user noinherit login password :'pg_ingest_password';
-alter role :pg_ingest_user set search_path = "$user", insight;
 create role :pg_worker_user noinherit login password :'pg_worker_password';
 alter role :pg_worker_user set search_path = "$user", insight;
 
 grant usage on schema insight to web_user;
-grant usage on schema insight to :pg_ingest_user;
 grant usage on schema insight to :pg_worker_user;
 
 create or replace function notify()
@@ -39,10 +34,11 @@ create table insight.pagestream (
     id uuid not null default gen_random_uuid(),
     path text not null,
     name text not null,
+    is_merged boolean not null default false,
     primary key (id)
 );
 grant all on insight.pagestream to web_user;
-grant all on insight.pagestream to :pg_ingest_user;
+grant all on insight.pagestream to :pg_worker_user;
 
 create trigger notify_pagestream
 after insert on insight.pagestream for each row 
@@ -59,7 +55,6 @@ create table insight.file (
     foreign key(pagestream_id) references insight.pagestream (id) match simple on delete restrict not valid
 );
 grant all on insight.file to web_user;
-grant all on insight.file to :pg_ingest_user;
 grant all on insight.file to :pg_worker_user;
 
 create trigger notify_file
@@ -76,10 +71,6 @@ create table insight.page (
     foreign key(pagestream_id) references insight.pagestream (id) match simple on delete restrict not valid
 );
 grant all on insight.page to web_user;
-grant all on insight.page to :pg_ingest_user;
-
-create trigger notify_page
-after insert on insight.page for each row 
-execute function notify();
+grant all on insight.page to :pg_worker_user;
 
 commit;
