317cf82524e8f17ff26f95fc57137f7cd2d8c5ad
Johan <johan@ftm.nl>
2023-11-15 12:47:40 +0100

    add llama-index and pgvector backend, add support for prompts

--- database/init.sql
+++ database/init.sql
@@ -4,22 +4,24 @@ begin;
 \set pg_api_password `echo $PG_API_PASSWORD`
 \set pg_worker_user `echo $PG_WORKER_USER`
 \set pg_worker_password `echo $PG_WORKER_PASSWORD`
-\set web_anon `echo $PG_ANONYMOUS`
 
 create schema insight;
+create schema private;
 
-create role :web_anon nologin;
+create extension if not exists vector;
+
+create role web_anon nologin;
 create role web_user nologin;
+grant usage on schema insight to web_user;
 
 create role :pg_api_user noinherit login password :'pg_api_password';
-grant :web_anon to :pg_api_user;
+grant web_anon to :pg_api_user;
 grant web_user to :pg_api_user;
 
 create role :pg_worker_user noinherit login password :'pg_worker_password';
 alter role :pg_worker_user set search_path = "$user", insight;
-
-grant usage on schema insight to web_user;
 grant usage on schema insight to :pg_worker_user;
+grant usage on schema private to :pg_worker_user;
 
 create or replace function notify()
 returns trigger language plpgsql as
@@ -72,5 +74,52 @@ create table insight.page (
 );
 grant all on insight.page to web_user;
 grant all on insight.page to :pg_worker_user;
+
+create table insight.prompt (
+    id uuid not null default gen_random_uuid(),
+    query text,
+
+    primary key (id)
+);
+grant all on insight.prompt to web_user;
+grant all on insight.prompt to :pg_worker_user;
+
+create trigger notify_prompt
+after insert on insight.prompt for each row 
+execute function notify();
+
+create table insight.response (
+    id uuid not null default gen_random_uuid(),
+    prompt_id uuid not null,
+    response text,
+
+    primary key (id),
+    foreign key(prompt_id) references insight.prompt (id) match simple on delete restrict not valid
+);
+grant all on insight.response to web_user;
+grant all on insight.response to :pg_worker_user;
+
+
+create table private.data_llamaindex (
+    id bigint not null,
+    text character varying not null,
+    metadata_ json,
+    node_id character varying,
+    embedding vector(1536)
+);
+grant all on private.data_llamaindex to :pg_worker_user;
+
+create sequence private.data_llamaindex_id_seq
+    start with 1
+    increment by 1
+    no minvalue
+    no maxvalue
+    cache 1;
+
+grant all on private.data_llamaindex_id_seq to :pg_worker_user;
+
+alter sequence private.data_llamaindex_id_seq owned by private.data_llamaindex.id;
+alter table private.data_llamaindex alter column id set default nextval('private.data_llamaindex_id_seq'::regclass);
+alter table private.data_llamaindex add constraint data_llamaindex_pkey primary key (id);
 
 commit;
