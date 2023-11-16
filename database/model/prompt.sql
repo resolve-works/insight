0a9ea6fb33de4f8825a5105ac25f1c0db919cb2d
Johan <johan@ftm.nl>
2023-11-16 03:12:49 +0100

    revert to requirements file to not break dependencies

--- database/model/prompt.sql
+++ database/model/prompt.sql
@@ -1,25 +1,25 @@
 
 create or replace function answer_prompt() returns trigger language plpython3u as $$
-import os
-from llama_index import VectorStoreIndex
-from llama_index.vector_stores import PGVectorStore
+    import os
+    from llama_index import VectorStoreIndex
+    from llama_index.vector_stores import PGVectorStore
 
-vector_store = PGVectorStore.from_params(
-    host="127.0.0.1",
-    port=5432,
-    user=os.environ.get('PG_WORKER_USER'),
-    password=os.environ.get("PG_WORKER_PASSWORD"),
-    database="insight",
-    schema_name="private",
-    table_name="page",
-    perform_setup=False,
-    embed_dim=1536,
-)
-vector_store_index = VectorStoreIndex.from_vector_store(vector_store)
-query_engine = vector_store_index.as_query_engine()
+    vector_store = PGVectorStore.from_params(
+        host="127.0.0.1",
+        port=5432,
+        user=os.environ.get('PG_WORKER_USER'),
+        password=os.environ.get("PG_WORKER_PASSWORD"),
+        database="insight",
+        schema_name="private",
+        table_name="page",
+        perform_setup=False,
+        embed_dim=1536,
+    )
+    vector_store_index = VectorStoreIndex.from_vector_store(vector_store)
+    query_engine = vector_store_index.as_query_engine()
 
-TD["new"]["response"] = query_engine.query(TD["new"]["query"])
-return "MODIFY"
+    TD["new"]["response"] = query_engine.query(TD["new"]["query"])
+    return "MODIFY"
 $$;
 
 create table if not exists public.prompt (
