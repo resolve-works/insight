149f102c55b03a9b6aab282af6b2ce545b2189f3
Johan <johan@ftm.nl>
2023-11-21 14:21:05 +0100

    add source document pages to prompts responses

--- database/model/prompt.sql
+++ database/model/prompt.sql
@@ -2,7 +2,6 @@
 create or replace function create_prompt(query text) returns json language plpython3u as $$
     import os
     import json
-    import logging
     from llama_index import VectorStoreIndex
     from llama_index.vector_stores import PGVectorStore
 
@@ -20,14 +19,20 @@ create or replace function create_prompt(query text) returns json language plpyt
     vector_store_index = VectorStoreIndex.from_vector_store(vector_store)
     query_engine = vector_store_index.as_query_engine()
     response = query_engine.query(query)
-    logging.info(response.source_nodes)
+
+    plan = plpy.prepare("insert into private.prompt (query, response) values ($1, $2) returning id", ["text", "text"])
+    prompts = plpy.execute(plan, [query, response.response])
 
     plan = plpy.prepare(
-        "insert into private.prompt (query, response) values ($1, $2) returning *",
-        ["text", "text"]
+        "insert into private.source (prompt_id, pagestream_id, index, score) values ($1, $2, $3, $4)",
+        ["uuid", "uuid", "integer", "float"]
     )
-    results = plpy.execute(plan, [query, response])
-    return json.dumps(results[0])
+    for node in response.source_nodes:
+        node = plpy.execute(
+            plan, 
+            [prompts[0]['id'], node.metadata['pagestream_id'], node.metadata['index'], node.get_score()]
+        )
+    return json.dumps(list(prompts))
 $$;
 
 create table if not exists private.prompt (
@@ -42,10 +47,16 @@ create table if not exists private.source (
     prompt_id uuid not null,
     pagestream_id uuid not null,
     index integer not null,
+    score float not null,
 
     constraint fk_prompt foreign key(prompt_id) references private.prompt(id) on delete cascade,
     constraint fk_pagestream foreign key(pagestream_id) references private.pagestream(id) on delete cascade
 );
 
-grant select, insert on private.prompt to web_user;
+create or replace view source as select * from private.source;
+create or replace view prompt as select * from private.prompt;
+
+grant select, update, insert on private.prompt to web_user;
 grant select, insert on private.source to web_user;
+grant select, insert on public.prompt to web_user;
+grant select, insert on public.source to web_user;
