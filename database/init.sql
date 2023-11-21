d5a4e7acfae44de8c3f6d1813b510e305d9198c5
Johan <johan@ftm.nl>
2023-11-21 18:16:19 +0100

    change tokens and roles

--- database/init.sql
+++ database/init.sql
@@ -10,12 +10,14 @@ create schema if not exists private;
 create extension if not exists vector;
 create extension if not exists plpython3u;
 
-create role web_anon nologin;
-create role web_user nologin;
+create role insight_anon nologin;
+create role insight_user nologin;
+create role insight_worker nologin;
 
 create role :pg_api_user noinherit login password :'pg_api_password';
-grant web_anon to :pg_api_user;
-grant web_user to :pg_api_user;
+grant insight_anon to :pg_api_user;
+grant insight_user to :pg_api_user;
+grant insight_worker to :pg_api_user;
 
 create role :pg_worker_user noinherit login password :'pg_worker_password';
 
@@ -36,14 +38,12 @@ alter sequence private.data_page_id_seq owned by private.data_page.id;
 alter table private.data_page alter column id set default nextval('private.data_page_id_seq'::regclass);
 alter table private.data_page add constraint data_page_pkey primary key (id);
 
-grant usage on schema public to web_user;
-grant all on all tables in schema public to web_user;
-grant usage, select on all sequences in schema public to web_user;
-grant usage on schema private to web_user;
-grant usage on schema public to :pg_worker_user;
-grant all on all tables in schema public to :pg_worker_user;
-grant usage, select on all sequences in schema public to :pg_worker_user;
+grant usage on schema public to insight_user;
+grant usage on schema private to insight_user;
+grant usage on schema public to insight_worker;
+grant usage on schema private to insight_worker;
 
+grant usage on schema public to :pg_worker_user;
 grant usage on schema private to :pg_worker_user;
 grant all on all tables in schema private to :pg_worker_user;
 grant usage, select on all sequences in schema private to :pg_worker_user;
