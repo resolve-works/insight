787745a7495944823a6bec977b36c324161077a0
Johan <johan@ftm.nl>
2023-11-29 02:55:03 +0100

    make minio hand out access credentials based on JWT and policies

--- minio/mc.sh
+++ minio/mc.sh
@@ -5,3 +5,4 @@ mc alias set insight $MINIO_ENDPOINT $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 mc mb insight/insight
 
 mc admin policy create insight external_user ./external_user.json
+mc admin policy create insight internal_worker ./internal_worker.json
