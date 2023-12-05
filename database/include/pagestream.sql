b16b4ae2f4b40556ec9d4a0818ac3338f64f464f
Johan <johan@ftm.nl>
2023-12-06 00:03:03 +0100

    set pagestream owner, allow users access to their S3 directory

--- database/include/pagestream.sql
+++ database/include/pagestream.sql
@@ -8,8 +8,19 @@ create or replace function ingest_pagestream(id uuid) returns void language plpy
     plpy.execute(plan)
 $$;
 
+create or replace function private.pagestream_set_owner() returns trigger language plpgsql as $$
+declare
+    owner_id uuid := current_setting('request.jwt.claims', true)::json->>'sub';
+begin
+    new.owner_id = owner_id;
+    return new;
+end
+$$;
+
 create table if not exists private.pagestream (
     id uuid default gen_random_uuid(),
+    owner_id uuid not null,
+    path text generated always as (owner_id::text || '/' || id::text || '.pdf') stored,
     name text not null,
     is_merged boolean not null default false,
     primary key (id)
@@ -18,3 +29,6 @@ grant select, insert on private.pagestream to external_user;
 
 create or replace view pagestream as select * from private.pagestream;
 grant insert, select on pagestream to external_user;
+
+create or replace trigger pagestream_set_owner before insert on private.pagestream for each row execute function private.pagestream_set_owner();
+
