b6a9d880ad7fe33ae23d7b20c94de339ce9f4441
Johan <johan@ftm.nl>
2024-01-25 18:27:06 +0100

    add ingestion indicator, s3 signing route

--- database/include/views.sql
+++ database/include/views.sql
@@ -1,10 +1,11 @@
 
 create or replace view pagestream as 
-    select id, name, path, status, created_at, updated_at from private.pagestream;
+    select id, owner_id, name, path, status, created_at, updated_at from private.pagestream;
 grant select, insert, update on pagestream to external_user;
+grant select, update on pagestream to internal_worker;
 
 create or replace view file as 
-    select id, pagestream_id, path, from_page, to_page, name from private.file;
+    select id, owner_id, pagestream_id, path, from_page, to_page, name from private.file;
 grant select on file to external_user;
 grant select, insert on file to internal_worker;
 
