16bf9943498840d9df469723664de4ca4a755a82
Johan <johan@ftm.nl>
2024-02-21 10:47:01 +0100

    tweak debezium and rabbitmq

--- minio/mc.sh
+++ minio/mc.sh
@@ -4,5 +4,11 @@ mc alias set insight $MINIO_ENDPOINT $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 
 mc mb insight/insight
 
+# Create policies (role bindings / permissions)
 mc admin policy create insight external_user ./external_user.json
 mc admin policy create insight internal_worker ./internal_worker.json
+
+# Create worker user for service account access
+mc admin user add insight $MINIO_WORKER_USER $MINIO_WORKER_PASSWORD
+mc admin policy attach insight internal_worker --user $MINIO_WORKER_USER
+
