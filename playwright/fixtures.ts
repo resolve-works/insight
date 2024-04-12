91381f18ca4f74c5c90f636d0faf00d0b6e7bbfa
Johan <johan.schuijt@ftm.nl>
2024-04-12 16:43:43 +0200

    add test oidc provider class, add test fixture

--- playwright/fixtures.ts
+++ playwright/fixtures.ts
@@ -1,92 +1,80 @@
 import { test as base } from '@playwright/test';
-import fs from 'fs';
-import path from 'path';
+import { randomUUID } from 'crypto';
 
-const get_admin_credentials = async () => {
-    const data = {
-        username: process.env.KEYCLOAK_ADMIN || '',
-        password: process.env.KEYCLOAK_ADMIN_PASSWORD || '',
-        grant_type: "password",
-        client_id: "admin-cli",
+class OIDCProvider {
+    access_token: string
+
+    async authenticate(username: string, password: string) {
+        const data = {
+            username,
+            password,
+            grant_type: "password",
+            client_id: "admin-cli",
+        }
+
+        const res = await fetch('https://localhost:8000/realms/master/protocol/openid-connect/token', {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/x-www-form-urlencoded' 
+            },
+            body: new URLSearchParams(data).toString(),
+        });
+
+        const { access_token } = await res.json()
+        this.access_token = access_token
     }
 
-    const res = await fetch('https://localhost:8000/realms/master/protocol/openid-connect/token', {
-        method: 'POST',
-        headers: { 
-            'Content-Type': 'application/x-www-form-urlencoded' 
-        },
-        body: new URLSearchParams(data).toString(),
-    });
+    create_user(username: string, password: string) {
+        const data = {
+            enabled: true,
+            username,
+            email: `${username}@example.com`,
+            firstName: username,
+            lastName: "insight",
+            credentials: [ { type: "password", value: password, temporary: false, } ]
+        }
 
-    return res.json()
+        return fetch('https://localhost:8000/admin/realms/insight/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${this.access_token}`,
+            },
+            body: JSON.stringify(data),
+        })
+    }
+
+    async delete_user(username: string) {
+        console.log(`deleting ${username}`)
+    }
 }
 
-const create_account = async id => {
-    const { access_token } = await get_admin_credentials()
+export const test = base.extend({
+    page: async ({ page, baseURL }, use) => {
+        if( ! baseURL ) {
+            throw new Error('baseURL unconfigured');
+        }
 
-    const username = `user-${id}`;
-    const password = "insight";
+        if( ! process.env.KEYCLOAK_ADMIN || ! process.env.KEYCLOAK_ADMIN_PASSWORD ) {
+            throw new Error('Keycloak admin credentials not set');
+        }
 
-    const data = {
-        enabled: true,
-        username,
-        email: `${username}@example.com`,
-        firstName: username,
-        lastName: "insight",
-        credentials: [
-            {
-                type: "password",
-                value: password,
-                temporary: false,
-            }
-        ]
+        const provider = new OIDCProvider()
+        await provider.authenticate(process.env.KEYCLOAK_ADMIN, process.env.KEYCLOAK_ADMIN_PASSWORD)
+
+        const username = randomUUID();
+        const password = "insight";
+        await provider.create_user(username, password)
+
+        await page.goto(baseURL);
+        await page.getByLabel('Username or email').fill(username)
+        await page.getByLabel('Password', { exact: true }).fill(password);
+        await page.getByRole('button', { name: 'Sign In' }).click();
+        await page.waitForURL(baseURL);
+
+        await use(page);
+        
+        await provider.delete_user(username)
     }
+})
 
-    const res = await fetch('https://localhost:8000/admin/realms/insight/users', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${access_token}`,
-        },
-        body: JSON.stringify(data),
-    })
-
-    return { username, password }
-}
-
-export * from '@playwright/test';
-export const test = base.extend<{}, { workerStorageState: string }>({
-  // Use the same storage state for all tests in this worker.
-  storageState: ({ workerStorageState }, use) => use(workerStorageState),
-
-  // Authenticate once per worker with a worker-scoped fixture.
-  workerStorageState: [async ({ browser }, use) => {
-    // Important: make sure we authenticate in a clean environment by unsetting storage state.
-    const id = test.info().parallelIndex;
-    const filename = path.resolve(test.info().project.outputDir, `.auth/${id}.json`);
-
-    if (fs.existsSync(filename)) {
-      await use(filename);
-      return;
-    }
-
-    const { username, password } = await create_account(id)
-
-    // Important: make sure we authenticate in a clean environment by unsetting storage state.
-    const page = await browser.newPage({ storageState: undefined, ignoreHTTPSErrors: true });
-
-    // Perform authentication steps. Replace these actions with your own.
-    await page.goto('http://localhost:3000');
-    // This 'test' user should exist in the insight_test OIDC realm
-    await page.getByLabel('Username or email').fill(username)
-    await page.getByLabel('Password', { exact: true }).fill(password);
-    await page.getByRole('button', { name: 'Sign In' }).click();
-
-    // Wait until the page receives the cookies.
-    await page.waitForURL('http://localhost:3000');
-
-    await page.context().storageState({ path: filename });
-    await page.close();
-    await use(filename);
-  }, { scope: 'worker' }],
-});
