49202aeaf742efd895f947be8dbd13497a258583
Johan Schuijt <johan.schuijt@ftm.nl>
2024-10-03 18:25:35 +0200

    test for corrupted file errors

--- tests/file_index.spec.ts
+++ tests/file_index.spec.ts
@@ -1,5 +1,6 @@
 
 import {test, expect} from '../playwright';
+import path from 'path';
 
 test('Create folder', async ({file_index_page, page}) => {
     await file_index_page.create_folder()
@@ -11,8 +12,13 @@ test('Upload file', async ({file_index_page, page}) => {
     await expect(page.getByTestId('inode-title')).toHaveCount(1)
 })
 
-test('Upload wrong file type')
+test('Upload wrong file type', async ({file_index_page, page}) => {
+    await file_index_page.upload_file(path.join(__dirname, 'file_index.spec.ts'))
+    await expect(page.getByTestId('inode-error')).toHaveAttribute('title', 'unsupported_file_type')
+})
 
-test('Upload corrupted file')
-
+test('Upload corrupted file', async ({file_index_page, page}) => {
+    await file_index_page.upload_file(path.join(__dirname, 'corrupted.pdf'))
+    await expect(page.getByTestId('inode-error')).toHaveAttribute('title', 'corrupted_file')
+})
 
