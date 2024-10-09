365be33fcafa4ddc47377d03678b8deaf07d4600
Johan Schuijt <johan.schuijt@ftm.nl>
2024-10-09 15:05:12 +0200

    basic conversation test

--- tests/conversation_detail.spec.ts
+++ tests/conversation_detail.spec.ts
@@ -2,7 +2,9 @@
 import {test, expect} from '../playwright';
 
 test('Answer prompt', async ({conversation_detail_page, page}) => {
-    const query = 'State purpose'
+    const query = 'Return a single word'
     await conversation_detail_page.prompt(query)
     await expect(page.getByTestId('human-message')).toContainText(query)
+    await expect(page.getByTestId('message-loader')).toHaveCount(1);
+    await expect(page.getByTestId('message-loader')).toHaveCount(0);
 })
