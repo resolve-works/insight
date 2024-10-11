f98c56cfebae270211292c3cd033d3ccd7abfca2
Johan Schuijt <johan.schuijt@ftm.nl>
2024-10-11 21:21:18 +0200

    update conversation tests for new streaming answers

--- tests/conversation_detail.spec.ts
+++ tests/conversation_detail.spec.ts
@@ -1,22 +1,19 @@
+import { test, expect } from '../playwright';
 
-import {test, expect} from '../playwright';
+test('Create prompt', async ({ empty_conversation_detail_page, page }) => {
+	const query = 'Return a single word';
+	await empty_conversation_detail_page.prompt(query);
+	await expect(page.getByTestId('human-message')).toContainText(query);
+	await expect(page.getByTestId('streamed-answer')).toHaveCount(0);
+	await expect(page.getByTestId('machine-message')).toHaveCount(2);
+});
 
-test('Create prompt', async ({empty_conversation_detail_page, page}) => {
-    const query = 'Return a single word'
-    await empty_conversation_detail_page.prompt(query)
-    await expect(page.getByTestId('human-message')).toContainText(query)
-    await expect(page.getByTestId('message-loader')).toHaveCount(1);
-    // OpenAI can be sloow
-    await expect(page.getByTestId('message-loader')).toHaveCount(0, {timeout: 10000});
-})
-
-test('Exceed context of embedding model', async ({empty_conversation_detail_page, page}) => {
-    const query = 'This is a test string, '
-    // Exceed openai gpt-4-turbo limit
-    await empty_conversation_detail_page.prompt(query.repeat(8192 / 5))
-    await expect(page.getByTestId('message-loader')).toHaveCount(0);
-    await expect(page.getByTestId('error-message')).toHaveCount(1)
-})
+test('Exceed context of embedding model', async ({ empty_conversation_detail_page, page }) => {
+	const query = 'This is a test string, ';
+	// Exceed openai limit
+	await empty_conversation_detail_page.prompt(query.repeat(8192 / 5));
+	await expect(page.getByTestId('error-message')).toHaveCount(1);
+});
 
 // TODO
 // I tested this locally with a big PDF because we need quite a bit of data in
