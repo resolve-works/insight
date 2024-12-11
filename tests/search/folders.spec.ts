8d73bc0013d53477ef4262c0ea30ff2fd350787e
Johan Schuijt <johan.schuijt@ftm.nl>
2024-12-11 14:17:44 +0100

    test search folderfilter options

--- tests/search/folders.spec.ts
+++ tests/search/folders.spec.ts
@@ -1,43 +1,37 @@
-import type { Page } from '@playwright/test';
-import { test as base, expect } from '../../playwright';
+import { test, expect } from '../../playwright';
 import { FileIndexPage } from '../../playwright/fixtures';
 
 export * from '@playwright/test';
 
-type Fixtures = {
-	search_index_page: Page;
-};
+test('load_folder_filter', async ({ file_index_page, page }) => {
+	// Create folders
+	const folder_paths = [
+		await file_index_page.create_folder('folder_1'),
+		await file_index_page.create_folder('folder_2'),
+		await file_index_page.create_folder('folder_3')
+	];
 
-export const test = base.extend<Fixtures>({
-	search_index_page: async ({ file_index_page, page }, use) => {
-		// Create folders
-		const folder_paths = [
-			await file_index_page.create_folder('folder 1'),
-			await file_index_page.create_folder('folder 2'),
-			await file_index_page.create_folder('folder 3')
-		];
+	const folder_detail_pages = folder_paths.map(
+		(folder_path) => new FileIndexPage(page, folder_path)
+	);
 
-		const folder_detail_pages = folder_paths.map(
-			(folder_path) => new FileIndexPage(page, folder_path)
-		);
+	// Upload a file in every folder and wait for them to be indexed
+	for (const folder_detail_page of folder_detail_pages) {
+		await folder_detail_page.goto();
+		await folder_detail_page.upload_file(folder_detail_page.FILE);
+		const inode = folder_detail_page.get_inode(folder_detail_page.FILE);
 
-		// Upload a file in every folder and wait for them to be indexed
-		for (const folder_detail_page of folder_detail_pages) {
-			await folder_detail_page.goto();
-			await folder_detail_page.upload_file(folder_detail_page.FILE);
-			const inode = folder_detail_page.get_inode(folder_detail_page.FILE);
-
-			await expect(inode.getByTestId('inode-loader')).toBeVisible();
-			await expect(inode.getByTestId('inode-loader')).not.toBeVisible();
-		}
-
-		await page.goto('/search');
-
-		// Use it as a index page
-		await use(page);
+		await expect(inode.getByTestId('inode-loader')).toBeVisible();
+		await expect(inode.getByTestId('inode-loader')).not.toBeVisible();
 	}
-});
 
-test('load_folder_filter', async ({ search_index_page }) => {
-	await expect(search_index_page.getByText('files found')).toBeVisible();
+	const all_folders_response = await page.request.get(`/search/folders`);
+	expect(all_folders_response.ok()).toBeTruthy();
+	const all_folders = await all_folders_response.json();
+	expect(all_folders.length).toBeGreaterThanOrEqual(3);
+
+	const query_folders_response = await page.request.get(`/search/folders?query=folder_1`);
+	expect(query_folders_response.ok()).toBeTruthy();
+	const query_folders = await query_folders_response.json();
+	expect(query_folders.length).toBeLessThan(all_folders.length);
 });
